bd65dfa5a884785bb4743059a5c1a33a9ed4a8e3
FlyingFandango <aaronsr132@gmail.com>
2026-09-23 13:06:18 +0200

    enumerado listo

---
+++ ruby/irrgarten/enumerados.rb
@@ -0,0 +1,19 @@
+#encoding:utf-8
+module Irrgarten
+  module Directions
+      LEFT = :left
+      RIGHT = :right
+      UP = :up
+      DOWN = :down
+  end
+
+  module Orientation
+    VERTICAL = :vertical
+    HORIZONTAL = :horizontal
+  end
+
+  module GameCharacter
+    PLAYER = :player
+    MONSTER = :monster
+  end
+end
