38fe28353abfd63929a8c058ab163dccfd475573
FlyingFandango <aaronsr132@gmail.com>
2026-09-23 19:30:18 +0200

    las otras 2 que me faltaban

--- ruby/irrgarten/shield.rb
+++ ruby/irrgarten/shield.rb
@@ -1,1 +1,26 @@
 #encoding:utf-8
+module Irrgarten
+  class Shield
+    def initialize(protection, uses)
+      @protection = protection
+      @uses = uses
+    end
+
+    def protect
+      if @uses > 0
+        @uses -= 1
+        return @protection
+      else
+        return 0.0
+      end
+    end
+
+    def to_s
+      "S[#{@protection}, #{@uses}]"
+    end
+
+    def discard
+      Dice.discard_element(@uses)
+    end
+  end
+end
--- ruby/irrgarten/weapon.rb
+++ ruby/irrgarten/weapon.rb
@@ -1,1 +1,27 @@
 #encoding:utf-8
+
+module Irrgarten
+  class Weapon
+    def initialize(power, uses)
+      @power = power
+      @uses = uses
+    end
+
+    def attack
+      if @uses > 0
+        @uses -= 1
+        return @power
+      else
+        return 0.0
+      end
+    end
+
+    def to_s
+      "W[#{@power}, #{@uses}]"
+    end
+
+    def discard
+      Dice.discard_element(@uses)
+    end
+  end
+end
