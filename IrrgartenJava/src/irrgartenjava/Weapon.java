3e50917ea95f547c4d0f6c6f678e9a3dea285825
franrdgz <franrdgz@correo.ugr.es>
2026-09-23 13:25:59 +0200

    creacion de weapon

---
+++ IrrgartenJava/src/irrgartenjava/Weapon.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgartenjava;
+
+/**
+ *
+ * @author fran-rdgz
+ */
+public class Weapon {
+    private float power;
+    private int uses; 
+    
+    
+    public Weapon (float p, int u){
+        power = p;
+        uses = u;
+    }
+    
+    public float attack (){
+        
+    }
+}
