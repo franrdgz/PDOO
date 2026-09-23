1fe419d66f2d208f122b9341f211ef763dbd49f7
franrdgz <franrdgz@correo.ugr.es>
2026-09-23 13:09:10 +0200

    Creacion de Directions

---
+++ IrrgartenJava/src/Directions/Directions.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Directions;
+
+/**
+ *
+ * @author fran-rdgz
+ */
+public enum Directions {
+    LEFT, RIGHT, UP, DOWN
+    
+}
